f8f690993d8309e7913d92b3174b20a54f806311
mukhtar <mukhtar@Code.panther>
2022-12-21 22:16:38 +0300

    correction

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,9 @@
-import getFullResponseFromAPI from "./1-promise";
-
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
